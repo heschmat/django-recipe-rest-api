705d77d5eb94d2d6d9e4f5da88290901c43e9a67
Hesch <heschmat.seyedi@gmail.com>
2024-08-15 13:15:51 +0200

    Verify email for newly created users.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -4,6 +4,13 @@ from django.test import TestCase
 
 from django.contrib.auth import get_user_model
 
+
+def normalize_email(email):
+    """Normalize the email by lowercasing the domain part."""
+    local, domain = email.split('@')
+    return f'{local}@{domain.lower()}'
+
+
 class ModelTests(TestCase):
     """Test models."""
 
@@ -11,8 +18,26 @@ class ModelTests(TestCase):
         """Test if creating a user with email is successful."""
         email = 'mai@example.com'
         password = 'changeme'
-        user = get_user_model().objects.create_user(email= email, password= password)
+        user = get_user_model().objects.create_user(email=email, password=password)
 
         self.assertEqual(user.email, email)
         # We check if the hash matches for password.
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Make sure emails for new users are normalized."""
+        sample_emails = [
+            'test1@EXAMPLE.com',
+            'Test2@Example.com',
+            'TEST3@EXAMPLE.COM',
+            'test4@example.COM',
+        ]
+
+        for email in sample_emails:
+            user = get_user_model().objects.create_user(email, password='1234')
+            self.assertEqual(user.email, normalize_email(email))
+
+    def test_new_user_without_email_raises_error(self):
+        """Creating a user w/o an email raises a ValueError."""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(email='', password='1234')
